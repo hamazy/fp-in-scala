f324d9f5a838cbd6165c56fe9c17d545ff33346c
Suguru Hamazaki <hamazy@webspiritus.com>
2013-11-01 00:18:58 +0900

    Update the version of scala, sbt, etc.

--- project/Build.scala
+++ project/Build.scala
@@ -1,19 +1,41 @@
 import sbt._
 import Keys._
 
+object BuildSettings {
+  val buildSettings = Defaults.defaultSettings ++ Seq(
+    organization := "com.suguruhamazaki",
+    version := "1.0-SNAPSHOT",
+    scalaVersion := "2.10.2",
+    scalacOptions ++= Seq("-deprecation", "-unchecked", "-feature", "-encoding", "utf8")
+  )
+}
+
+object Dependencies {
+  val scalaLogging = "com.typesafe"        %% "scalalogging-slf4j" % "1.0.1"
+  val logback = "ch.qos.logback" % "logback-classic" % "1.0.13"
+  val scalatest = "org.scalatest"       %% "scalatest"          % "2.0.M8" % "test"
+  val pegdown = "org.pegdown" % "pegdown" % "1.0.1" % "test"
+  val mockito = "org.mockito"         % "mockito-core"        % "1.9.5" % "test"
+
+  val commonDeps = Seq(
+    scalaLogging,
+    logback,
+    scalatest,
+    pegdown,
+    mockito
+  )
+}
+
 object FpInScalaBuild extends Build {
 
-  override lazy val settings =
-    super.settings ++ Seq(
-      scalaVersion := "2.10.0",
-      scalacOptions ++= Seq("-deprecation", "-unchecked", "-feature"),
-      libraryDependencies ++= Seq(
-	"org.scala-lang" % "scala-actors" % "2.10.0",
-	"com.typesafe" %% "scalalogging-slf4j" % "1.0.1",
-        "org.scalatest" %% "scalatest" % "1.9.1" % "test",
-        "org.mockito" % "mockito-core" % "1.9.5" % "test")
-    )
+  import BuildSettings.buildSettings
+  import Dependencies.commonDeps
+  import org.scalastyle.sbt.ScalastylePlugin.{Settings => ScalastyleSettings}
+  import ScctPlugin.instrumentSettings
 
-  lazy val root = Project(id = "fp-in-scala",
-			  base = file("."))
+  lazy val root = Project("fp-in-scala",
+			  file("."),
+                          settings = buildSettings ++ instrumentSettings ++ ScalastyleSettings) settings (
+                    libraryDependencies ++= commonDeps,
+                    testOptions in Test += Tests.Argument(TestFrameworks.ScalaTest, "-h", "target/report", "-o"))
 }
