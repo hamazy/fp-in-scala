1a8971b7a36312612ecbe1870dd3943e146f95d5
Suguru Hamazaki <hamazy@suguruhamazaki.com>
2014-11-16 12:41:22 +0900

    Add exercise 2, 3, 5 and 6 of ch10.

--- src/main/scala/com/suguruhamazaki/ch10/Exercise.scala
+++ src/main/scala/com/suguruhamazaki/ch10/Exercise.scala
@@ -29,17 +29,32 @@ object Monoid {
     def op(a1: Boolean, a2: Boolean): Boolean = a1 & a2
     def zero: Boolean = false
   }
+  // ex2
+  def optionMonoid[A]: Monoid[Option[A]] = new Monoid[Option[A]] {
+    def op(a1: Option[A], a2: Option[A]): Option[A] =
+      a1.flatMap(_ => a2) // a2.flatMap(_ => a1)
+    def zero: Option[A] = None
+  }
+  // ex3
+  def endoMonoid[A]: Monoid[A => A] = new Monoid[A => A] {
+    def op(a1: A => A, a2: A => A): A => A = a1.compose(a2)
+    def zero: A => A = a => a
+  }
 }
 
 trait Foldable[F[_]] {
   // ex6
   // in terms of foldMap
-  def foldRight[A, B](as: F[A])(z: B)(f: (A, B) => B): B = ???
+  def foldRight[A, B](as: F[A])(z: B)(f: (A, B) => B): B =
+    foldMap(as)(f.curried)(Monoid.endoMonoid)(z)
+
   // ex6
   // in terms of foldMap
-  def foldLeft[A, B](as: F[A])(z: B)(f: (B, A) => B): B = ???
+  def foldLeft[A, B](as: F[A])(z: B)(f: (B, A) => B): B =
+    foldMap(as)({ (a: A, b: B) => f(b, a) }.curried)(Monoid.endoMonoid)(z)
   // ex5
   // in terms of foldRight or foldLeft
-  def foldMap[A, B](as: F[A])(f: A => B)(mb: Monoid[B]): B = ???
+  def foldMap[A, B](as: F[A])(f: A => B)(mb: Monoid[B]): B =
+    foldRight(as)(mb.zero) { (a, b) => mb.op(f(a), b) }
 
 }
