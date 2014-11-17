8fcc59862aa1adf814c45a212cf8acdfe574c772
Suguru Hamazaki <hamazy@suguruhamazaki.com>
2014-11-18 07:58:09 +0900

    Add exercise 1 and 2 of ch13.

---
+++ src/main/scala/com/suguruhamazaki/ch13/Exercise.scala
@@ -0,0 +1,86 @@
+package com.suguruhamazaki.ch13
+
+import com.suguruhamazaki.ch12.Monad
+import scala.language.higherKinds
+
+package stackcunsuming {
+
+  import scala.io.StdIn
+
+  sealed trait IO[A] { self =>
+    def run: A
+    def map[B](f: A => B): IO[B] =
+      new IO[B] { def run = f(self.run) }
+    def flatMap[B](f: A => IO[B]): IO[B] =
+      new IO[B] { def run = f(self.run).run }
+  }
+
+  object IO extends Monad[IO] {
+    def unit[A](a: => A): IO[A] = new IO[A] { def run = a }
+    override def flatMap[A, B](fa: IO[A])(f: A => IO[B]) = fa.flatMap(f)
+    def apply[A](a: => A): IO[A] = unit(a)
+  }
+
+  object Converter {
+    def fahrenheitToCelsius(f: Double): Double =
+      (f - 32) * 5.0 / 9.0
+    def ReadLine: IO[String] = IO { StdIn.readLine }
+    def PrintLine(msg: String): IO[Unit] = IO { println(msg) }
+
+    def converter: IO[Unit] = for {
+      _ <- PrintLine("Enter a temprature in degree Fahrenheit:")
+      d <- ReadLine.map(_.toDouble)
+      _ <- PrintLine(fahrenheitToCelsius(d).toString)
+    } yield ()
+  }
+
+}
+
+package stackfree {
+  sealed trait IO[A] {
+    def flatMap[B](f: A => IO[B]): IO[B] =
+      FlatMap(this, f)
+    def map[B](f: A => B): IO[B] =
+      flatMap(f.andThen(Return(_)))
+  }
+
+  case class Return[A](a: A) extends IO[A]
+  case class Suspend[A](resume: () => A) extends IO[A]
+  case class FlatMap[A, B](sub: IO[A], k: A => IO[B]) extends IO[B]
+
+}
+
+sealed trait Free[F[_], A] {
+  // ex1
+  def map[B](f: A => B): Free[F, B] =
+    flatMap(f.andThen(Return(_)))
+  def flatMap[B](f: A => Free[F, B]): Free[F, B] =
+    FlatMap(this, f)
+}
+case class Return[F[_], A](a: A) extends Free[F, A]
+case class Suspend[F[_], A](s: F[A]) extends Free[F, A]
+case class FlatMap[F[_], A, B](s: Free[F, A], f: A => Free[F, B]) extends Free[F, B]
+
+object Free {
+  type TailRec[A] = Free[Function0, A]
+
+  // ex1
+  def freeMonad[F[_]] = new Monad[({ type f[a] = Free[F, a] })#f] {
+    def unit[A](a: => A): Free[F, A] = Return(a)
+    override def flatMap[A, B](fa: Free[F, A])(f: A => Free[F, B]): Free[F, B] =
+      fa.flatMap(f)
+  }
+
+  // ex2
+  @annotation.tailrec
+  def runTranpoline[A](a: Free[Function0, A]): A =
+    a match {
+      case Return(x) => x
+      case Suspend(f) => f()
+      case FlatMap(x, f) => x match {
+        case Return(y) => runTranpoline(f(y))
+        case Suspend(g) => runTranpoline(f(g()))
+        case FlatMap(y, g) => runTranpoline(y.flatMap(z => g(z).flatMap(f)))
+      }
+    }
+}
